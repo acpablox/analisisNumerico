fd469fce3fabd6e138949bf7c68e2f3bdf4b35a0
acpablox <85261167+acpablox@users.noreply.github.com>
2023-05-29 18:15:44 -0500

    Update Home.py

--- Analisis/Home.py
+++ Analisis/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import sympy as sy
+import sympy as sp
 st.title('Solución numérica de ecuaciones no lineales, solución de sistemas de ecuaciones e interpolación')
 
 st.subheader('Esta página web fue diseñada con el fin de resolver problemas acerca de los diferentes temas tratados en el semestre de una manera intuitiva y clara, de tal manera que quien le dé uso tenga claridad de la solución obtenida y de sus errores en caso tal de tenerlos.')
