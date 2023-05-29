2f4bc5bbc9eed1c71ac7342f9218aef8f8aebcb1
acpablox <85261167+acpablox@users.noreply.github.com>
2023-05-29 16:40:00 -0500

    Update RaicesMultiples.py

--- Analisis/pages/RaicesMultiples.py
+++ Analisis/pages/RaicesMultiples.py
@@ -16,42 +16,42 @@ f = st.text_input('Ingrese la funcion')
 if f:
     x = sp.symbols('x')
     fun = sp.sympify(f)
-# Inicia el método de raíces múltiples
+    # Inicia el método de raíces múltiples
 
-E =[]
-N = []
-xn = []
-fn = []
+    E =[]
+    N = []
+    xn = []
+    fn = []
 
-x0 = a
-x1 = 0
-eval0 = fun.subs(x,x0)
-dfun = fun.diff(x)
-eval1 = dfun.evalf(subs = {x:x0})
-d2fun = dfun.diff(x)
-eval2 = d2fun.evalf(subs = {x:x0})
-error = 1
-E.append(error)
-c = 0
-N.append(c)
-xn.append(x0)
-fn.append(eval0)
-while error > tol and c<=niter:
-    x1 = x0 - (eval0*eval1)/((eval1**2)-eval0*eval2)
-    error = abs(x1-x0)
-    x0 = x1
-    eval0 = fun.evalf(subs = {x:x0})
-    eval1 = dfun.evalf(subs={x:x0})
-    eval2 = d2fun.evalf(subs={x:x0})
-    c += 1
+    x0 = a
+    x1 = 0
+    eval0 = fun.subs(x,x0)
+    dfun = fun.diff(x)
+    eval1 = dfun.evalf(subs = {x:x0})
+    d2fun = dfun.diff(x)
+    eval2 = d2fun.evalf(subs = {x:x0})
+    error = 1
     E.append(error)
+    c = 0
     N.append(c)
     xn.append(x0)
     fn.append(eval0)
-if error<=tol:
-    st.write(x1,'es una aproximacion de la raiz multiple')
-else:
-    st.write('Fracaso en',niter,'iteraciones')
-d = {'Iteración': N,'Xn':xn,'fn':fn,'Error':E}
-tabla = pd.DataFrame(data = d)
-st.table(tabla)
+    while error > tol and c<=niter:
+        x1 = x0 - (eval0*eval1)/((eval1**2)-eval0*eval2)
+        error = abs(x1-x0)
+        x0 = x1
+        eval0 = fun.evalf(subs = {x:x0})
+        eval1 = dfun.evalf(subs={x:x0})
+        eval2 = d2fun.evalf(subs={x:x0})
+        c += 1
+        E.append(error)
+        N.append(c)
+        xn.append(x0)
+        fn.append(eval0)
+    if error<=tol:
+        st.write(x1,'es una aproximacion de la raiz multiple')
+    else:
+        st.write('Fracaso en',niter,'iteraciones')
+    d = {'Iteración': N,'Xn':xn,'fn':fn,'Error':E}
+    tabla = pd.DataFrame(data = d)
+    st.table(tabla)
