d0d12b5e73c5160e2b9711a937550c7e77409a13
acpablox <85261167+acpablox@users.noreply.github.com>
2023-05-29 23:31:15 -0500

    Update Biseccion.py

--- Analisis/pages/Biseccion.py
+++ Analisis/pages/Biseccion.py
@@ -80,8 +80,8 @@ else:
 	st.write("El intervalo es inadecuado")
 
 # Create a range of x values for the graph
-xs = Xs +20
-xi = Xi -20
+xs = Xs +5
+xi = Xi -5
 x_vals = np.linspace(xi, xs, 100)
 y_vals = [eval(Fun) for x in x_vals]
 
